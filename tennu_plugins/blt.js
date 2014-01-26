30820ce0a85b84a99362b88142c715e100cdafad
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2014-01-25 19:34:05 -0800

    Readme

---
+++ tennu_plugins/blt.js
@@ -0,0 +1,23 @@
+var BLTPlugin = {
+    init: function (client, imports) {
+        return {
+            handlers: {
+                '!givemeblt': function (command) {
+                    client.act(command.channel, 'gives a juicy BLT to ' + command.nickname);
+                }
+            },
+
+            help: {
+                'givemeblt': [
+                    '!givemeblt',
+                    'Gives the requestor a juicy BLT.'
+                ]
+            },
+
+            commands: ['givemeblt']
+        }
+    }
+};
+
+module.exports = BLTPlugin;
+
